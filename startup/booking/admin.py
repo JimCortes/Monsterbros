8b44de486ab33c539f56561015058bbd0a209cba
jim <jimcortesemail@gmail.com>
2024-01-12 23:09:39 -0800

    pricing

--- startup/booking/admin.py
+++ startup/booking/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from .models import Branch, Truck, Appointment, Notes, Product, Quote
+
+admin.site.register(Branch)
+admin.site.register(Truck)
+admin.site.register(Appointment)
+admin.site.register(Notes)
+admin.site.register(Product)
+admin.site.register(Quote)
--- startup/booking/models.py
+++ startup/booking/models.py
@@ -36,6 +36,7 @@ class Appointment(models.Model):
         ('Cancelled', 'Cancelled'),
     ]
     id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
     truck = models.ForeignKey(Truck, on_delete=models.CASCADE, blank=True, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -51,10 +52,47 @@ class Appointment(models.Model):
 
 class Notes(models.Model):
     id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     appoiment_id = models.ForeignKey(Appointment, on_delete=models.CASCADE)
     user = models.CharField(max_length=50)
     notes = models.CharField(max_length=250, blank=True)
     
     def __str__(self):
-        return  {self.id , self.note} 
+        return  {self.id , self.notes} 
+
+class Product(models.Model):
+    STATUS_CHOICES = [
+        ('Expire', 'Expire'),
+        ('Active', 'Active'),
+    ]
+    
+    CURRENCIES = [
+        ('CAD','CAD'),
+        ('MXN','MXN'),
+        ('USA','USA'),
+    
+    ]
+
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    product = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3, default='CAD', choices=CURRENCIES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    
+    
+    def __str__(self):
+        return self.product
+
+
+
+
+class Quote(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+   
 
